106bd70e359a26ebe01fa25c7e8a00e426cd129d
NikoFellner <niko.fellner@gmx.de>
2026-03-01 21:21:45 +0100

    add second class

---
+++ DuelBase/src/character/mage.py
@@ -0,0 +1,13 @@
+from src.character.base.base_character import BaseCharacter
+
+
+class Mage(BaseCharacter):
+
+    def attack(self) -> int:
+        return self.attack_power * 2
+
+    def defend(self) -> int:
+        return self.armor * 0.5
+
+    def heal(self) -> int:
+        return self._health + 5
