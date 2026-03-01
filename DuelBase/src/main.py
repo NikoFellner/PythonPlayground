79a571fad20e06860df64fb33eccd26d87b67e12
NikoFellner <niko.fellner@gmx.de>
2026-03-01 21:22:13 +0100

    WIP

--- DuelBase/src/main.py
+++ DuelBase/src/main.py
@@ -1,5 +1,20 @@
+from src.character.mage import Mage
 from src.character.warrior import Warrior
+from src.services.game_service import Game
+from src.services.user_interface_service import UserInterface
 
-x = Warrior()
+if __name__ == "__main__":
+    mage = Mage()
+    warrior = Warrior()
 
-print(x.health)
+    while True:
+        print(f"Mage health: {mage.health}")
+        print(f"Warrior healt: {warrior.health}")
+
+        dmg = mage.attack()
+        warrior.health -= dmg
+        dmg = warrior.attack()
+        mage.health -= dmg
+
+        if mage.health <= 0 or warrior.health <= 0:
+            break
