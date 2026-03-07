aeb237f3871419685325a0f73a5b3be844296e75
NikoFellner <niko.fellner@gmx.de>
2026-03-07 09:44:10 +0100

    adding base stats for mage and warrior

--- DuelBase/src/character/mage.py
+++ DuelBase/src/character/mage.py
@@ -2,6 +2,12 @@ from src.character.base.base_character import BaseCharacter
 
 
 class Mage(BaseCharacter):
+    def __init__(self):
+        super().__init__()
+        self._health = 80
+        self._armor = 10
+        self.armor_class = 20
+
     def attack(self) -> int:
         return self.attack_power * 2
 
--- DuelBase/src/character/warrior.py
+++ DuelBase/src/character/warrior.py
@@ -4,6 +4,9 @@ from src.character.base.base_character import BaseCharacter
 class Warrior(BaseCharacter):
     def __init__(self):
         super().__init__()
+        self._health = 120
+        self._armor = 20
+        self.armor_class = 10
 
     def attack(self) -> int:
         return self.attack_power
