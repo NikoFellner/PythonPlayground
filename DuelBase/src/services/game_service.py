8d682aa034ec8617754d13816f76bca67ea9dd03
NikoFellner <niko.fellner@gmx.de>
2026-03-08 09:56:31 +0100

    start game service

--- DuelBase/src/services/game_service.py
+++ DuelBase/src/services/game_service.py
@@ -1,3 +1,10 @@
+from src.character.enemy.enemy_factory import EnemyFactory
+from src.character.hero.hero import Hero
+from src.services.user_interface import UserInterface
+
+
 class Game:
     def __init__(self):
-        pass
+        self.ui = UserInterface()
+        self.enemy_factory = EnemyFactory()
+        self.hero_factory = Hero()
