0bed7ab94251f3365b804ff5cd78c4e90a4ab363
NikoFellner <niko.fellner@gmx.de>
2026-03-14 11:40:46 +0100

    fix di (WIP)

--- DuelBase/src/services/dependency_injection.py
+++ DuelBase/src/services/dependency_injection.py
@@ -5,14 +5,18 @@ from src.character.hero.hero import Hero
 from src.character.hero.hero_type import HeroConfig
 from src.services.game_service import Game
 
+from src.user_interface.ui_config import UIConfig
+
 
 class DependencyInjection:
-    def __init__(self, hero_config: HeroConfig, enemy_config: EnemyConfig):
+    def __init__(self, hero_config: HeroConfig, enemy_config: EnemyConfig, ui_config: UIConfig):
         self.hero_config = hero_config
         self.enemy_config = enemy_config
+        self.__ui_config = ui_config
 
         self.__hero = self._create_hero(hero_config)
         self.__enemy_factory = self._create_enemy_factory(enemy_config)
+        self.__game = self._create_game(self.__hero, self.__enemy_factory, self.enemy_config)
 
     @property
     def hero(self) -> BaseCharacter:
@@ -23,13 +27,21 @@ class DependencyInjection:
         return self.__enemy_factory
 
     @property
-    def game(self):
-        return Game(hero=self.hero, enemy_factory=self.enemy_factory)
+    def game(self) -> Game:
+        return self.__game
+
+    @property
+    def ui_config(self) -> UIConfig:
+        return self.__ui_config
+
+    @staticmethod
+    def _create_game(hero:BaseCharacter, enemy:EnemyFactory, enemy_config: EnemyConfig):
+        return Game(hero=hero, enemy_factory=enemy, enemy_config=enemy_config)
 
     @staticmethod
     def _create_hero(hero_config: HeroConfig) -> BaseCharacter:
         return Hero.create_hero(hero_config)
 
     @staticmethod
-    def _create_enemy_factory(enemy_config: EnemyConfig) -> EnemyFactory:
+    def _create_enemy_factory(enemy_config:EnemyConfig) -> EnemyFactory:
         return EnemyFactory(enemy_config)
